7805be8f336c47b712ebf474ebb0bc1e232dea7e
krisboyce-orthofi <krisboyce-orthofi@bitbucket.org>
2016-12-19 23:51:56 -0700

    Work Done

---
+++ Logic/PeiceMovement.cs
@@ -0,0 +1,60 @@
+﻿using Model;
+using Model.Constants;
+
+namespace Logic
+{
+    public static class PeiceMovement
+    {
+        public static bool CanMove(Peice peice, int dX, int dY)
+        {
+            switch (peice.Type)
+            {
+                case PeiceType.Pawn:
+                    return PawnCanMove(peice, dX, dY);
+            }
+            return false;
+        }
+
+        private static bool PawnCanMove(Peice peice, int dX, int dY)
+        {
+            var board = Board.getInstance();
+            var validMove = false;
+            if (peice.Type != PeiceType.Pawn)
+                return false;
+            validMove = (peice.Side == board.Top && dY < 0) || (peice.Side == board.Bottom && dY > 0);
+
+            if (!validMove)
+                return validMove;
+
+            if(dX != 0)
+            {
+                Peice capturePeice = board.GetPeice(peice.X + dX, peice.Y + dY);
+                if (peice != null)
+                {
+                    if (peice.Side != capturePeice.Side)
+                        return true;
+                    else
+                        return false;
+                }
+
+                Peice enPassantPeice = board.GetPeice(peice.X + dX, peice.Y);
+                if (enPassantPeice.Type != PeiceType.Pawn)
+                    return false;
+
+                if (peice.Type == enPassantPeice.Type)
+                    return false;
+
+                if (!enPassantPeice.HasMoved)
+                    return false;
+
+                if(enPassantPeice.Side == board.Top && enPassantPeice.Y == 3) {
+                    return true;
+                }else if(enPassantPeice.Side == board.Bottom && enPassantPeice.Y == 4)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
---
+++ Logic/TurnLogic.cs
@@ -0,0 +1,74 @@
+﻿using Model;
+using Model.Constants;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logic
+{
+    public static class TurnLogic
+    {
+        public static TurnCommand GetCommand(Player player, string commandString)
+        {
+            var command = ParseCommandString(commandString);
+            command.player = player;
+            var valid = command.ValidateArguments();
+            return valid ? command : null;
+        }
+
+        private static TurnCommand ParseCommandString(string commandString)
+        {
+            var command = new TurnCommand();
+            var commandComponents = commandString.Split(' ').ToList();
+            switch (commandComponents.First().ToLower())
+            {
+                case "help":
+                case "h":
+                    command.Type = TurnType.Help;
+                    break;
+                case "move":
+                case "m":
+                    command.Type = TurnType.Move;
+                    break;
+                case "quit":
+                case "q":
+                    command.Type = TurnType.Quit;
+                    break;
+                case "save":
+                case "s":
+                    command.Type = TurnType.Save;
+                    break;
+                case "load":
+                case "l":
+                    command.Type = TurnType.Load;
+                    break;
+                default:
+                    command.Type = TurnType.Help;
+                    break;
+            }
+            return command;
+        }
+        
+        private static bool ValidateArguments(this TurnCommand command)
+        {
+
+            return false;
+        }
+
+        public static bool ExecuteCommand(TurnCommand command)
+        {
+            switch (command.Type)
+            {
+
+            }
+            return false;
+        }
+
+        public static void HelpAction(string command)
+        {
+
+        }
+    }
+}
